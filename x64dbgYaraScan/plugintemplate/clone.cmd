e77cee84c423d481a3b944a4b289b298d0e8cbb7
共产主义接班人 <503407184@qq.com>
2025-04-16 17:30:04 +0800

    Update clone.cmd

--- x64dbgYaraScan/plugintemplate/clone.cmd
+++ x64dbgYaraScan/plugintemplate/clone.cmd
@@ -1,23 +1,22 @@
 @echo off
 
+cd /D %~dp0
 
-cd %~dp0
-
-:: x64dbg version
-set snapshot=snapshot_2023-08-01_14-50
-
-set GIT=https://github.com/x64dbg/PluginTemplate/
+set X64TEMPLATE=https://github.com/x64dbg/PluginTemplate/
 
 setlocal enabledelayedexpansion
 
 set PLUGIN[0]=plugin
 set PLUGIN[1]=pluginmain
 
+
 :: plugintemplate
-for /l %%i in (0, 1, 1) do ( curl -L %GIT%blob/main/src/!PLUGIN[%%i]!.h?raw=true -o !PLUGIN[%%i]!.h && curl -L %GIT%blob/main/src/!PLUGIN[%%i]!.cpp?raw=true -o !PLUGIN[%%i]!.cpp )
+for /l %%i in (0, 1, 1) do ( curl -L %X64TEMPLATE%blob/main/src/!PLUGIN[%%i]!.h?raw=true -o !PLUGIN[%%i]!.h && curl -L %X64TEMPLATE%blob/main/src/!PLUGIN[%%i]!.cpp?raw=true -o !PLUGIN[%%i]!.cpp )
 
 
 :: pluginsdk
-curl -L https://github.com/x64dbg/x64dbg/releases/download/snapshot/%snapshot%.zip -o x64dbg.zip
+:: https://api.github.com/repos/x64dbg/x64dbg/releases/latest
+set SNAPSHOT=snapshot_2023-06-10_18-05
+curl -L https://github.com/x64dbg/x64dbg/releases/download/snapshot/%SNAPSHOT%.zip -o x64dbg.zip
 tar -xf x64dbg.zip pluginsdk/
 del /Q x64dbg.zip
